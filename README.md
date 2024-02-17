725d8cde0892f846db5f1134f4bde86288109326
Rangga056 <muhammadrangga056@gmail.com>
2024-02-17 08:34:06 +0700

    feat(readme): Adding readme.md file feat(about): Update the experience section

--- README.md
+++ README.md
@@ -1,36 +1,65 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# My Personal Web Portfolio
 
-## Getting Started
+Welcome to my personal web portfolio! This website showcases my projects, skills, and experiences.
 
-First, run the development server:
+## Table of Contents
+
+- [Visit My Portfolio](#visit-my-portfolio)
+- [About](#about)
+- [Features](#features)
+- [Technologies Used](#technologies-used)
+- [Setup](#setup)
+- [Usage](#usage)
+- [Deployment](#deployment)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Visit My Portfolio
+
+You can visit my portfolio [here](https://eclipse-dev-portfolio.vercel.app).
+
+
+## About
+
+This web portfolio is built using Next.js, a popular React framework for building modern web applications. It's designed to be minimalistic, user-friendly, and responsive across various devices.
+
+## Features
+
+- Clean and modern design
+- Responsive layout for mobile and desktop
+- Showcase of projects with descriptions and links
+- Contact form for reaching out
+
+## Technologies Used
+
+- **Next.js**: Framework for building React applications.
+- **React**: JavaScript library for building user interfaces.
+- **CSS Modules**: For styling components with scoped styles.
+- **TailwindCSS**: Utility-first CSS framework.
+- **EmailJS**: a backend-as-a-service that gives you the tools to send emails.
+- **Vercel**: Platform for deploying serverless functions and static sites.
+
+## Setup
+
+1. Clone the repository: `git clone https://github.com/your-username/your-portfolio.git`
+2. Navigate to the project directory: `cd your-portfolio`
+3. Install dependencies: `npm install`
+
+## Usage
+
+To start the development server:
 
 ```bash
 npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
 ```
+## Deployment
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+This project is deployed using Vercel. Simply push your changes to the main branch, and Vercel will automatically deploy the updates.
 
-You can start editing the page by modifying `app/page.js`. The page auto-updates as you edit the file.
+## Contributing
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+Contributions are welcome! If you find any issues or have suggestions for improvements, please open an issue or create a pull request.
 
-## Learn More
+## License
 
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -147,106 +147,113 @@ const AboutPage = () => {
             >
               Experience
             </motion.h1>
-            <motion.div
+            <motion.span
+            initial={{ x: "-300px" }}
+            animate={isExperienceRefInView ? { x: "0" } : {}}
+            className="italic text-lg text-gray-500"
+            >
+              Currently i have no proffesional experience yet
+            </motion.span>
+            {/* <motion.div 
               initial={{ x: "-300px" }}
               animate={isExperienceRefInView ? { x: "0" } : {}}
             >
-              {/* EXPERIENCE LIST ITEM */}
+             
               <div className="flex justify-between h-48">
-                {/* LEFT */}
+               
                 <div className="w-1/3 ">
-                  {/* JOB TITLE */}
+                 
                   <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
                     Senior JavaScript Engineer
                   </div>
-                  {/* JOB DESC */}
+                  
                   <div className="p-3 text-sm italic">
                     I led web development, offering expertise in JavaScript
-                    frameworks.{" "}
+                    frameworks.
                   </div>
-                  {/* JOB DATE */}
+                  
                   <div className="p-3 text-red-400 text-sm font-semibold">
                     2024 - Present
                   </div>
-                  {/* JOB COMPANY */}
+                 
                   <div className="p-1 rounded bg-white text-sm font-semibold w-fit">
                     Apple
                   </div>
                 </div>
-                {/* CENTER */}
+               
                 <div className="w-1/7 flex justify-center">
-                  {/* LINE */}
+                 
                   <div className="w-1 h-full bg-gray-600 rounded relative">
-                    {/* LINE CIRCLE */}
+                    
                     <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
                   </div>
                 </div>
-                {/* RIGHT */}
+             
                 <div className="w-1/3 "></div>
               </div>
-              {/* EXPERIENCE LIST ITEM */}
+              
               <div className="flex justify-between h-48">
-                {/* LEFT */}
+             
                 <div className="w-1/3 "></div>
-                {/* CENTER */}
+                
                 <div className="w-1/6 flex justify-center">
-                  {/* LINE */}
+                 
                   <div className="w-1 h-full bg-gray-600 rounded relative">
-                    {/* LINE CIRCLE */}
+                   
                     <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
                   </div>
                 </div>
-                {/* RIGHT */}
+              
                 <div className="w-1/3 ">
-                  {/* JOB TITLE */}
+                
                   <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
                     Senior React Developer
                   </div>
-                  {/* JOB DESC */}
+            
                   <div className="p-3 text-sm italic">
                     I spearheaded React-based application development,
-                    leveraging advanced skills.{" "}
+                    leveraging advanced skills.
                   </div>
-                  {/* JOB DATE */}
+                 
                   <div className="p-3 text-red-400 text-sm font-semibold">
-                    2019 - 2024{" "}
+                    2019 - 2024
                   </div>
-                  {/* JOB COMPANY */}
+                 
                   <div className="p-1 rounded bg-white text-sm font-semibold w-fit">
                     Microsoft
                   </div>
                 </div>
               </div>
-              {/* EXPERIENCE LIST ITEM */}
+             
               <div className="flex justify-between h-48">
-                {/* LEFT */}
+                
                 <div className="w-1/3 ">
-                  {/* JOB TITLE */}
+                  
                   <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
                     Freelancer{" "}
                   </div>
-                  {/* JOB DESC */}
+                
                   <div className="p-3 text-sm italic">
                     I provided web solutions, applying a range of technologies
                     to address client requirements.{" "}
                   </div>
-                  {/* JOB DATE */}
+                
                   <div className="p-3 text-red-400 text-sm font-semibold">
                     2010 - 2019{" "}
                   </div>
                 </div>
-                {/* CENTER */}
+         
                 <div className="w-1/6 flex justify-center">
-                  {/* LINE */}
+                
                   <div className="w-1 h-full bg-gray-600 rounded relative">
-                    {/* LINE CIRCLE */}
+                    
                     <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
                   </div>
                 </div>
-                {/* RIGHT */}
+                
                 <div className="w-1/3 "></div>
               </div>
-            </motion.div>
+            </motion.div> */}
             {/*ganti ke motion div*/}
           </div>
         </div>
