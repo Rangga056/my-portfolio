026cab94a1a98f73de8e154a9e502c579f01308f
Rangga056 <muhammadrangga056@gmail.com>
2024-03-19 08:59:53 +0700

    Update
    Add new portfolio

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -9,13 +9,21 @@ const items = [
   {
     id: 1,
     color: "from-red-100 to-blue-100",
+    title: "Airbnb Clone",
+    desc: "An Airbnb clone aimed at providing a platform for users to list, discover, and book accommodations worldwide.",
+    img: "https://github.com/Rangga056/airbnb-clone/assets/136163122/d7f64e59-450c-403c-b068-71db1b6c6d4a",
+    link: "https://airbnb-clone-ebon-nine.vercel.app/",
+  },
+  {
+    id: 2,
+    color: "from-red-100 to-blue-100",
     title: "Eclipse Summit",
     desc: "Eclipse Summit is an advanced event management web application that empowers users to seamlessly organize and participate in events.",
     img: "https://github.com/Rangga056/eclipse-summit/assets/136163122/235fb459-8f4e-489e-91c1-d541e96aa0b4",
     link: "https://eclipse-summit.vercel.app/",
   },
   {
-    id: 2,
+    id: 3,
     color: "from-blue-100 to-violet-100",
     title: "Eclipse Estate",
     desc: "Eclipse Estate is a user-friendly web application designed to streamline real estate transactions. Whether you're buying, renting, or selling properties like houses and villas, Eclipse Estate provides a seamless experience.",
@@ -23,7 +31,7 @@ const items = [
     link: "https://github.com/Rangga056/eclipse-estate",
   },
   {
-    id: 3,
+    id: 4,
     color: "from-violet-100 to-purple-100",
     title: "Eclipse Movie Web",
     desc: "A simple movie website using HTML, CSS, and Javascript with the themoviedb API",
@@ -31,7 +39,7 @@ const items = [
     link: "https://rangga056.github.io/eclipse-movie-app/",
   },
   {
-    id: 4,
+    id: 5,
     color: "from-purple-100 to-red-100",
     title: "Eclipse Weather App",
     desc: "A simple weather app using HTML, CSS, and Javascript with openweather API",
@@ -53,11 +61,29 @@ const PortfolioPage = () => {
       transition={{ duration: 1 }}
     >
       <div className="h-[600vh] relative" ref={ref}>
-        <div className="w-scren h-[calc(100dvh-6rem)] flex items-center justify-center text-6xl lg:text-8xl text-center">
+        <div className="w-scren h-[calc(100dvh-6rem)] flex flex-col md:gap-y-8 gap-y-16 items-center justify-center text-6xl lg:text-8xl text-center">
           {" "}
           My Works
+          <motion.svg
+            initial={{ opacity: 0.2, y: 0 }}
+            animate={{ opacity: 1, y: "10px" }}
+            transition={{ repeat: Infinity, duration: 3, ease: "easeInOut" }}
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            width={50}
+            height={50}
+          >
+            <path
+              d="M5 15C5 16.8565 5.73754 18.6371 7.05029 19.9498C8.36305 21.2626 10.1435 21.9999 12 21.9999C13.8565 21.9999 15.637 21.2626 16.9498 19.9498C18.2625 18.6371 19 16.8565 19 15V9C19 7.14348 18.2625 5.36305 16.9498 4.05029C15.637 2.73754 13.8565 2 12 2C10.1435 2 8.36305 2.73754 7.05029 4.05029C5.73754 5.36305 5 7.14348 5 9V15Z"
+              stroke="#000000"
+              strokeWidth="1"
+            ></path>
+            <path d="M12 6V14" stroke="#000000" strokeWidth="1"></path>
+            <path d="M15 11L12 14L9 11" stroke="#000000" strokeWidth="1"></path>
+          </motion.svg>
         </div>
-        <div className="sticky top-0 flex h-screen gap-6 items-center overflow-x-scroll snap-x">
+        <div className="sticky top-0 flex h-screen gap-6 items-center overflow-x-scroll snap-center">
           <motion.div style={{ x }} className="flex">
             <div className="h-screen w-screen flex items-center justify-center bg-gradient-to-r from-purple-100 to-red-100" />
             {items.map((item) => (
@@ -82,7 +108,7 @@ const PortfolioPage = () => {
                       />
                     </div>
                   </div>
-                  <div className="flex flex-col gap-3 items-start justify-center h-full">
+                  <div className=" flex flex-col gap-3 items-start justify-center h-full">
                     <p className="w-80 md:w-96 lg:w-[500px] lg:text-lg xl:w-[600px]">
                       {item.desc}
                     </p>
